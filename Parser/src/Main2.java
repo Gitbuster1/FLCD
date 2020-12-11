da5af04ffeedfc92568cd8996166c5941ddb4541
Gitbuster1 <63590473+Gitbuster1@users.noreply.github.com>
2020-12-11 13:01:15 +0200

    Add files via upload

--- Parser/src/Main2.java
+++ Parser/src/Main2.java
@@ -3,20 +3,20 @@ import Utils.ParserOutput;
 import recursiveDescendant.RecursiveDescendant;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
         try {
-            Grammar grammar = Grammar.fromFile("g1.txt");
+            Grammar grammar = Grammar.fromFile("g2.txt");
             System.out.println("Grammar:\n");
             printGrammar(grammar);
 
             RecursiveDescendant algorithm = new RecursiveDescendant(grammar);
-            List<String> seq = sequenceFromFile("seq.txt");
+            List<String> seq = sequenceFromPIF("PIF.out");
             System.out.println("Sequence: " + seq);
 
             List<String> productionString = algorithm.run(seq);
@@ -24,7 +24,7 @@ public class Main {
             parserOutput.addProductionString(productionString);
 
             System.out.println(parserOutput);
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("out1.txt"));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("out2.txt"));
             bufferedWriter.write(parserOutput.toString());
             bufferedWriter.close();
         } catch (Exception e) {
@@ -32,17 +32,22 @@ public class Main {
         }
     }
 
-    public static List<String> sequenceFromFile(String fileName) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)));
-        return Arrays.asList(scanner.nextLine().split(" "));
+    public static List<String> sequenceFromPIF(String pifFileName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new BufferedReader(new FileReader(pifFileName)));
+        List<String> w = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            w.add(line.substring(line.indexOf('(') + 1, line.indexOf(',')));
+        }
+        return w;
     }
 
     public static void printGrammar(Grammar grammar) {
         System.out.println("Nonterminals:" + grammar.getNonterminals() + "\n");
         System.out.println("Terminals:" + grammar.getTerminals() + "\n");
         System.out.println("Productions:\n" + grammar.getProductions() + "\n");
-        System.out.println("Production for S:");
-        System.out.println(grammar.getForProduction("S"));
+        System.out.println("Production for expression:");
+        System.out.println(grammar.getForProduction("expression"));
         System.out.println();
     }
 }
--- Parser/src/Utils/Grammar.java
+++ Parser/src/Utils/Grammar.java
@@ -1,8 +1,6 @@
 package Utils;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -21,57 +19,69 @@ public class Grammar {
         start = "";
     }
 
-    public static Grammar fromFile(String fileName) throws FileNotFoundException {
+    public static Grammar fromFile(String fileName) throws Exception {
         Grammar grammar = new Grammar();
-        Scanner sc = new Scanner(new BufferedReader(new FileReader(new File(fileName))));
+        Scanner sc = new Scanner(new BufferedReader(new FileReader(fileName)));
         String nonterminalsLine = sc.nextLine();
         grammar.nonterminals = Arrays.asList(nonterminalsLine.split(" "));
         String terminalsLine = sc.nextLine();
-        grammar.terminals = Arrays.asList(terminalsLine.split(" "));
+        grammar.terminals = new ArrayList<>(Arrays.asList(terminalsLine.split(" ")));
         grammar.start = sc.nextLine();
         String line;
-        for(String nonterminal : grammar.nonterminals){
+        for (String nonterminal : grammar.nonterminals) {
             grammar.productions.put(nonterminal, new ArrayList<>());
         }
-        while (sc.hasNext()){
+        while (sc.hasNext()) {
             line = sc.nextLine();
             String[] parts = line.split("->");
             String[] productions = parts[1].split("\\|");
-            for(String production: productions){
+            for (String production : productions) {
                 String[] symbols = production.split("#");
                 grammar.productions.get(parts[0]).add(Arrays.asList(symbols));
             }
 
         }
         sc.close();
+        if(!grammar.validate()){
+            throw new Exception("Invalid grammar");
+        }
         return grammar;
     }
 
-    public boolean validate(){
-        if (!nonterminals.contains(start)){
+    public boolean validate() {
+        if (!nonterminals.contains(start)) {
             return false;
         }
-        for(String key : productions.keySet()){
-            if(!nonterminals.contains(key)){
+        for (String key : productions.keySet()) {
+            if (!nonterminals.contains(key)) {
                 return false;
+            }
+        }
+        for (List<List<String>> rhs: productions.values()){
+            for (List<String> values: rhs){
+                for (String value: values){
+                    if(!(nonterminals.contains(value) || terminals.contains(value))){
+                        return false;
+                    }
+                }
             }
         }
         return true;
     }
 
-    public List<String> getNonterminals(){
+    public List<String> getNonterminals() {
         return this.nonterminals;
     }
 
-    public List<String> getTerminals(){
+    public List<String> getTerminals() {
         return this.terminals;
     }
 
-    public String getProductions(){
+    public String getProductions() {
         return productions.keySet().stream().map(this::getForProduction).collect(Collectors.joining("\n"));
     }
 
-    public String getForProduction(String nonterminal){
+    public String getForProduction(String nonterminal) {
         List<List<String>> productions = this.productions.get(nonterminal);
         StringBuilder sb = new StringBuilder();
         sb.append(nonterminal).append("->");
